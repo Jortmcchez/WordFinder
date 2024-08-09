9fd6f4fe9f63c05322784a728af07ec9666961a6
Jerrit Smith <126747440+Jortmcchez@users.noreply.github.com>
2024-08-09 17:31:24 -0600

    refactored a new word finder. testing board setups

--- newwordfinder.py
+++ newwordfinder.py
@@ -1,17 +1,15 @@
 import json
 
-def findword(letters, boardLetter, location, wordLength):
+def checkValid(word, dictionary):
+    validWord = True
+    for char in dictionary:
+        if word.count(char) > dictionary[char]:
+            validWord = False
+    return validWord
 
-# inputs:
-# letters - the letters available to you (include repeats)
-# boardLetter -  the letter on the board you want to make a word with (can be multiple letters)
-# location - the position you want the board letter to be at in the word (eg: 1st position, second position, etc)
-# wordLength - the length you want the word to be
 
-# output
-# an array of words that can be made based on the inputs
+def findword(letters, boardLetter, spaceLeft, spaceRight):
     
-    # creating a dictionary to keep track of letters and their repetitions
     letterDict = {}
     for char in letters:
         if char in letterDict:
@@ -23,35 +21,22 @@ def findword(letters, boardLetter, location, wordLength):
             letterDict[char] += 1
         else:
             letterDict.update({char:1})
-         
-    # Load the JSON file
+
     with open('words_dictionary.json', 'r') as file:
         data = json.load(file)
 
-    # Assuming data is a list of words
     filtered_words = [word for word in data 
-                      if len(word) > location-1 
-                      and word[location-1].lower() == boardLetter[0]
-                      and len(word) <= wordLength
-                      and set(word.lower()).issubset(letters + boardLetter)
-                      and boardLetter in word
-                      and checkValid(word, letterDict)
+                if boardLetter in word
+                and set(word.lower()).issubset(letters + boardLetter)
+                and checkValid(word, letterDict)
+                and spaceLeft - word.rfind(boardLetter) >= 0
+                and spaceRight - (len(word) - (word.rfind(boardLetter) + 1)) >= 0
     ]
-
-    return filtered_words
-
-# a function that checks the filtered words meet repetition requirements
-def checkValid(word, dictionary):
-    validWord = True
-    for char in dictionary:
-        if word.count(char) > dictionary[char]:
-            validWord = False
-    return validWord
-
-# a test print
-##print(findword("ltfnvsaeiouy", "j", 1, 4))
+    
+    return filtered_words            
 
 
+#print(findword("viclohe", "k", 2, 2))
 
 def checkRight(row, col):
     right_limit = 0
@@ -107,7 +92,7 @@ for row in arr:
             #print(right)
             #print(item)
             #print(letters)
-            print(findword(letters, item, 1, right))
+            print(findword(letters, item, left, right))
 
 
 
